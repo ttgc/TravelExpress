bd6a75e4a214a7644fd92e72f4dcdc90f7aa2eda
ttgc <ttgc@users.noreply.github.com>
2020-06-02 10:14:12 -0400

    Setup project

---
+++ Database/Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Travel_Express.Database
+{
+    public partial class Context : DbContext
+    {
+        public Context()
+        {
+        }
+
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Travel_Express.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
