7a2785debd02aff47dcae878cd6a73dd33dfcd51
Timothee Corsini <timothee.corsini@gmail.com>
2020-06-09 00:56:55 -0400

    added a Login page, TODO

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Travel_Express.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
