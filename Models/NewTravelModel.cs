09d70f2fd8f1eef7c24e9711eed7935366cbc269
Machin Truc <damien.auteur@live.fr>
2020-06-10 04:42:45 +0200

    Added the form and method to add a travel

---
+++ Models/NewTravelModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Travel_Express.Models
+{
+    public class NewTravelModel
+    {
+        public NewTravelModel()
+        {
+            
+        }
+
+       
+        public String FromStreet { get; set; }
+        public String FromComp { get; set; }
+        public String FromPostalCode { get; set; }
+        public String time1 { get; set; }
+        public String date1 { get; set; }
+        public String Depart { get; set; }
+        public String ToStreet { get; set; }
+        public String ToComp { get; set; }
+        public String ToPostalCode { get; set; }
+        public String time2 { get; set; }
+        public String date2 { get; set; }
+        public String Arrival { get; set; }
+        //public DateTime? TimeStart { get; set; }
+        //public DateTime? TimeEnd { get; set; }
+        public int Seats { get; set; }
+
+       
+    }
+}
